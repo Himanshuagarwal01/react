07d8878646bf8596cbcddbf78103a4ac0806abed
Himanshuagarwal01 <himanshu.agarwal@deeporion.com>
2023-04-19 17:09:22 +0530

    writing markup with jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Div from "./component/Div"; 
 import Button from "./component/Button";
+import Aboutpage from "./component/Aboutpage";
 
 function MyApp(){
   return (
   <>
   <Div/>
   <Button/>
+  <Aboutpage/>
   </>
 
   )
---
+++ src/component/Aboutpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Aboutpage(){
+    return (
+        <>
+        <h1>
+            About
+        </h1>
+        <p>
+           <b> hello  there</b>, <br/> how do you do?
+        </p>
+        </>
+    )
+}
+export default Aboutpage;
